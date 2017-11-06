4be00689fb96276cd3f30098186a98881d744890
Austin Passy <austin@passy.co>
2017-11-06 15:48:29 -0800

    Initial JS for AJAX submission.

--- assets/js/licensemanager.js
+++ assets/js/licensemanager.js
@@ -1,7 +1,33 @@
-/* global jQuery, emailDownload */
+/* global jQuery, ajaxurl, EddLicenseManager */
 (function ($) {
-  "use strict";
+    "use strict";
 
-  $(document).ready(function () {
-  });
+    $(document).ready(function () {
+        $('input#EddSoftwareLicenseManagerButton').on('submit', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+
+            $.ajax({
+                method: "POST",
+                url: ajaxurl,
+                data: {
+                    action: EddLicenseManager.action,
+                    nonce: EddLicenseManager.nonce
+                },
+                beforeSend: function () {
+                    $this.attr('disabled', true);
+                },
+                success: function (response) {
+                    if (typeof response.success !== 'undefined' && response.success) {
+                        $this.slideUp();
+                        document.location = response.url;
+                    }
+                    $this.attr('disabled', false);
+                },
+                fail: function () {
+                    alert('Unknown Error');
+                }
+            });
+        });
+    });
 }(jQuery));
