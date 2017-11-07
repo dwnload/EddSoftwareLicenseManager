069aff6f8aa665ecb1911346106dcd5bedf891ce
Austin Passy <austin@passy.co>
2017-11-07 11:43:00 -0800

    Update CSS & JS for license calls to the EDD Api store.

--- assets/js/licensemanager.js
+++ assets/js/licensemanager.js
@@ -3,29 +3,33 @@
     "use strict";
 
     $(document).ready(function () {
-        $('input#EddSoftwareLicenseManagerButton').on('submit', function (e) {
+        $('input#EddSoftwareLicenseManagerButton').on('click', function (e) {
             e.preventDefault();
-            var $this = $(this);
+            var $this = $(this),
+                img = 'EddLicenseLoader';
 
             $.ajax({
                 method: "POST",
                 url: ajaxurl,
                 data: {
                     action: EddLicenseManager.action,
-                    nonce: EddLicenseManager.nonce
+                    license_key: $('input[name="'+ EddLicenseManager.license_attr +'"]').val(),
+                    nonce: EddLicenseManager.nonce,
+                    plugin_action: $this.prop('name')
                 },
                 beforeSend: function () {
                     $this.attr('disabled', true);
+                    $('<img class="' + img + '" src="'+ EddLicenseManager.loading + '" height="16" width="16">').insertAfter($this);
                 },
                 success: function (response) {
                     if (typeof response.success !== 'undefined' && response.success) {
-                        $this.slideUp();
-                        document.location = response.url;
+                        location.reload(true);
                     }
                     $this.attr('disabled', false);
+                    $('img[class="'+ img +'"]').remove();
                 },
                 fail: function () {
-                    alert('Unknown Error');
+                    window.alert('Unknown Error');
                 }
             });
         });
