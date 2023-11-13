9dfac9a24c766fde83786f44f0661d94fa06f8ff
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-11-13 15:38:52 -0800

    Update JS formatting to newer standards.

--- assets/js/licensemanager.js
+++ assets/js/licensemanager.js
@@ -1,37 +1,36 @@
 /* global jQuery, ajaxurl, EddLicenseManager */
 (function ($) {
-    "use strict";
+  'use strict'
 
-    $(document).ready(function () {
-        $('input#EddSoftwareLicenseManagerButton').on('click', function (e) {
-            e.preventDefault();
-            var $this = $(this),
-                img = 'EddLicenseLoader';
+  $(document).ready(function () {
+    $('input#EddSoftwareLicenseManagerButton').on('click', function (e) {
+      e.preventDefault()
+      const $this = $(this)
 
-            $.ajax({
-                method: "POST",
-                url: ajaxurl,
-                data: {
-                    action: EddLicenseManager.action,
-                    license_key: $('input[name="'+ EddLicenseManager.license_attr +'"]').val(),
-                    nonce: EddLicenseManager.nonce,
-                    plugin_action: $this.prop('name')
-                },
-                beforeSend: function () {
-                    $this.attr('disabled', true);
-                    $('<img class="' + img + '" src="'+ EddLicenseManager.loading + '" height="16" width="16">').insertAfter($this);
-                },
-                success: function (response) {
-                    if (typeof response.success !== 'undefined' && response.success) {
-                        location.reload(true);
-                    }
-                    $this.attr('disabled', false);
-                    $('img[class="'+ img +'"]').remove();
-                },
-                fail: function () {
-                    window.alert('Unknown Error');
-                }
-            });
-        });
-    });
-}(jQuery));
+      $.ajax({
+        method: 'POST',
+        url: ajaxurl,
+        data: {
+          action: EddLicenseManager.action,
+          license_key: $('input[name="' + EddLicenseManager.license_attr + '"]').val(),
+          nonce: EddLicenseManager.nonce,
+          plugin_action: $this.prop('name')
+        },
+        beforeSend: function () {
+          $this.attr('disabled', true)
+          $('<img class="EddLicenseLoader" src="' + EddLicenseManager.loading + '" height="16" width="16">').insertAfter($this)
+        },
+        success: function (response) {
+          if (typeof response.success !== 'undefined' && response.success) {
+            location.reload(true)
+          }
+          $this.attr('disabled', false)
+          $('img[class="EddLicenseLoader"]').remove()
+        },
+        fail: function () {
+          window.alert('Unknown Error')
+        }
+      })
+    })
+  })
+}(jQuery))
