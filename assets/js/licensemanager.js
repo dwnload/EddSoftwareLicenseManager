e01d8a8236297b951b91aedbd92d24af4349afd1
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-11-14 11:00:27 -0800

    Update the JS to trigger license checks.

--- assets/js/licensemanager.js
+++ assets/js/licensemanager.js
@@ -3,26 +3,33 @@
   'use strict'
 
   $(document).ready(function () {
-    $('input#EddSoftwareLicenseManagerButton').on('click', function (e) {
+    $('a[id^="EddSoftwareLicenseManagerButton"]').on('click', function (e) {
       e.preventDefault()
       const $this = $(this)
+      const $element = $('input[name$="[' + $this.data('plugin_id') + ']"]')
+
+      if ($element.val().length === 0) {
+        return
+      }
 
       $.ajax({
         method: 'POST',
         url: ajaxurl,
         data: {
           action: EddLicenseManager.action,
-          license_key: $('input[name="' + EddLicenseManager.license_attr + '"]').val(),
+          license_key: $element.val(),
           nonce: EddLicenseManager.nonce,
-          plugin_action: $this.prop('name')
+          plugin_action: $this.data('action'),
+          plugin_id: $this.data('plugin_id')
         },
         beforeSend: function () {
           $this.attr('disabled', true)
           $('<img class="EddLicenseLoader" src="' + EddLicenseManager.loading + '" height="16" width="16">').insertAfter($this)
         },
         success: function (response) {
+          console.log(response)
           if (typeof response.success !== 'undefined' && response.success) {
-            location.reload(true)
+            location.reload()
           }
           $this.attr('disabled', false)
           $('img[class="EddLicenseLoader"]').remove()
