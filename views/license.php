e38b7405ca6d25c2c679f46a55b11ca90a4ee1f0
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-04-23 13:56:14 -0700

    Cleanup license view on activate, deactivate, and check license.

--- views/license.php
+++ views/license.php
@@ -11,14 +11,14 @@ if (!($this instanceof LicenseManager)) {
     wp_die();
 }
 
-if (!isset($plugin_id) || !isset($section_id)) {
+if (!isset($plugin_id, $section_id)) {
     return;
 }
 $license_key = Options::getOption($plugin_id, $section_id);
 $license_data = get_option(AbstractLicenceManager::LICENSE_SETTING, []);
 $license_expires = $license_data[$plugin_id]['expires'] ?? '';
 $license_status = $license_data[$plugin_id]['status'] ?? LicenseStatus::LICENSE_INACTIVE;
-
+$active_or_valid = in_array($license_status, [LicenseStatus::LICENSE_ACTIVE, LicenseStatus::LICENSE_VALID], true);
 ob_start();
 
 echo "<div class='EddSoftwareLicenseManager'>";
@@ -26,31 +26,36 @@ printf('License Status: <span class="license-status %1$s">%1$s</span>', $license
 if (!empty($license_expires)) {
     printf(
         ' &mdash; License Expires: <span class="license-expires">%1$s</span>',
-        date_i18n(get_option('date_format'), strtotime($license_expires))
+        $license_expires === 'lifetime' ?
+            'Never (lifetime)' :
+            date_i18n(get_option('date_format'), strtotime($license_expires))
     );
 }
 
 echo '<br><hr>';
 
-if (!empty($license_data) && $license_status === LicenseStatus::LICENSE_ACTIVE) {
-    $this->buildSubmitButton(
-        $plugin_id,
-        'button-primary',
-        LicenseStatus::LICENSE_DEACTIVATE,
-        $license_status,
-    );
+$this->buildSubmitButton(
+    $plugin_id,
+    'button-primary',
+    LicenseStatus::LICENSE_ACTIVATE,
+    $license_status
+);
+
+echo '&nbsp;&nbsp;';
+
+$this->buildSubmitButton(
+    $plugin_id,
+    $active_or_valid ? 'button-primary' : 'button-secondary disabled',
+    $license_status === LicenseStatus::LICENSE_DEACTIVATED ? LicenseStatus::LICENSE_ACTIVATE : LicenseStatus::LICENSE_DEACTIVATE,
+    $license_status,
+);
+
+if (!$active_or_valid) {
     echo '&nbsp;&nbsp;';
     $this->buildSubmitButton(
         $plugin_id,
         'button-secondary',
         LicenseStatus::LICENSE_CHECK_LICENSE,
-        $license_status
-    );
-} else {
-    $this->buildSubmitButton(
-        $plugin_id,
-        empty($license_key) ? 'button-primary disabled' : 'button-primary',
-        LicenseStatus::LICENSE_ACTIVATE,
         $license_status
     );
 }
